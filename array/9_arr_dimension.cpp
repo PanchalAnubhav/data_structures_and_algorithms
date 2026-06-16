0e06605774e8877449b5c170353cb4c3dbf8d4a7
ANUBHAV PANCHAL <panchalganubhav@gmail.com>
2026-06-16 16:46:49 +0530

    basic array and strings

---
+++ array/9_arr_dimension.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[1][2];
+    cin >> a[0][0] >> a[0][1];
+    cout << a[0][0] << " " << a[0][1];
+}
---
+++ basics/7_switch.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;    
+
+int main() {
+    int x;
+    cin >> x;
+    switch (x) {
+        case 1:
+            cout << "one";
+            break;
+        case 2:
+            cout << "two";
+            break;
+        case 3:
+            cout << "three";
+            break;
+        default:
+            cout << "invalid input";
+    }
+    return 0;
+}
