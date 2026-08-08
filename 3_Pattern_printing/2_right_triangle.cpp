3fb3cd13335f959da1799ee2839fa0b1408663fd
ANUBHAV PANCHAL <panchalganubhav@gmail.com>
2026-08-08 22:12:25 +0530

    add implementation of no_inc_row_wise function and update input values

--- 3_Pattern_printing/2_right_triangle.cpp
+++ 3_Pattern_printing/2_right_triangle.cpp
@@ -21,6 +21,27 @@ void star_only(){
     }
 }
 
+//---------------------------------------------------------------------------
+/*
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+*/
+
+void no_inc_row_wise(){
+    cout << "enter row value:" << "\n";
+    int n;
+    cin >> n;
+    for (int i = 1 ; i <= n ; i++){
+        for (int j = 1 ; j <= i ; j++){
+            cout << j  << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -28,6 +49,7 @@ int main(){
     cin >> t;
     while(t--){
         star_only();
+        no_inc_row_wise();
     }
     return 0;
 }
