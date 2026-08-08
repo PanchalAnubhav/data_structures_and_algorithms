daa4eb4e6964602568fc5fb4ac0c5b462244c4e0
ANUBHAV PANCHAL <panchalganubhav@gmail.com>
2026-08-08 22:30:30 +0530

    fix: correct loop indices in star_only function and add lower_wrt_column function

--- 3_Pattern_printing/2_right_triangle.cpp
+++ 3_Pattern_printing/2_right_triangle.cpp
@@ -14,8 +14,8 @@ void star_only(){
     cout << "enter row value:" << "\n";
     int n;
     cin >> n;
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < i ; j++){
+    for (int i = 1 ; i <= n ; i++){
+        for (int j = 1 ; j <= i ; j++){
             cout << "* ";
         }
         cout << "\n";
@@ -61,6 +61,45 @@ void no_inc_column_wise(){
     }
 }
 
+/*---------------------------------------------------------------------------
+5 5 5 5 5 
+4 4 4 4 
+3 3 3 
+2 2 
+1 
+*/
+
+void lower_wrt_column(){
+    cout << "enter row value:" << "\n";
+    int n;
+    cin >> n;
+    for (int i = n ; i >=1 ; i--){
+        for (int j = 1 ; j <= i ; j++){
+            cout << i  << " ";
+        }
+        cout << "\n";
+    }
+}
+
+/*---------------------------------------------------------------------------
+5 5 5 5 5 
+4 4 4 4 
+3 3 3 
+2 2 
+1 
+*/
+
+void lower_wrt_column(){
+    cout << "enter row value:" << "\n";
+    int n;
+    cin >> n;
+    for (int i = n ; i >=1 ; i--){
+        for (int j = 1 ; j <= i ; j++){
+            cout << i  << " ";
+        }
+        cout << "\n";
+    }
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -70,6 +109,7 @@ int main(){
         star_only();
         no_inc_row_wise();
         no_inc_column_wise();
+        lower_with_stars();
     }
     return 0;
 }
