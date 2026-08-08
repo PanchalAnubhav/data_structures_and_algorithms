6a4221b3ff716704cee8b6e6d194a76a6888877b
ANUBHAV PANCHAL <panchalganubhav@gmail.com>
2026-08-08 21:55:31 +0530

    refactor: remove old pattern printing implementation and add new pattern printing and right triangle functions

--- 3_Pattern_printing/2_right_triangle.cpp
+++ 3_Pattern_printing/2_right_triangle.cpp
@@ -1,12 +1,20 @@
  #include<bits/stdc++.h>
 using namespace std;
 
-void pattern_print(){
+/*
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
+
+void star_only(){
     cout << "enter row value:" << "\n";
     int n;
     cin >> n;
     for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < n ; j++){
+        for (int j = 0 ; j < i ; j++){
             cout << "* ";
         }
         cout << "\n";
@@ -19,7 +27,7 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        pattern_print();
+        star_only();
     }
     return 0;
 }
